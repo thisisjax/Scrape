cb6fc9ab057e62a6cc25d737128299b39b093286
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-09-18 15:26:04 -0400

    commit mongo

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/week18Populate
 ​
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true
 });
 
 // // Connect to the Mongo DB
